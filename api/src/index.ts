cbd2250a566a2ab7927032e3075ac0d3607569bd
GennadySX <htcucell@gmail.com>
2020-05-13 23:50:52 +0300

    maks-chat --user:%user --%message

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,9 @@ import logger from '@globals/Logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
+
+
+
 app.listen(port, () => {
     logger.info('Express server port => : ' + port);
 });
